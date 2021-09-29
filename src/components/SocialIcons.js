b43ee8eaaa6a26b4ac03f61526bc35bfd052d8dc
Troy Dorman <troydorman@gmail.com>
2021-09-29 09:38:53 -0600

    installed react-icons and displayed our social icons in our footer.

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,19 @@
+import { FaTwitter, FaFacebook, FaLinkedin } from "react-icons/fa";
+
+export default function SocialIcons() {
+  return (
+    <div>
+      <li>
+        <a href="https://twitter.com">
+          <FaTwitter />
+        </a>
+        <a href="https://Facebook.com">
+          <FaFacebook />
+        </a>
+        <a href="https://Linkedin.com">
+          <FaLinkedin />
+        </a>
+      </li>
+    </div>
+  );
+}
