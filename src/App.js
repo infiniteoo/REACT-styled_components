1f5e6da1ba0e126b8581175e9974c178792737a1
Troy Dorman <troydorman@gmail.com>
2021-09-29 09:14:43 -0600

    loop thru content array to display titles with .map()

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
 import { ThemeProvider } from "styled-components";
 import GlobalStyles from "./components/styles/Global";
+import content from "./content";
 
 const theme = {
   colors: {
@@ -19,7 +20,9 @@ function App() {
         <GlobalStyles />
         <Header />
         <Container>
-          <h1>hello world</h1>
+          {content.map((item, index) => (
+            <p>{item.title}</p>
+          ))}
         </Container>
       </>
     </ThemeProvider>
