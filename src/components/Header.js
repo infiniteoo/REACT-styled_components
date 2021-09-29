5891a55e31d14a07bb966a1dda0a5e601aa24d0c
Troy Dorman <troydorman@gmail.com>
2021-09-29 08:34:17 -0600

    you can also pass css values via props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import { StyledHeader } from "./styles/Header.styled";
 export default function Header() {
   return (
-    <StyledHeader>
+    <StyledHeader bg='red'>
       <h1>Hubble</h1>
     </StyledHeader>
   );
--- src/components/styles/Header.styled.js
+++ src/components/styles/Header.styled.js
@@ -1,14 +1,6 @@
 import styled from "styled-components";
 
 export const StyledHeader = styled.header`
-  background-color: #ebfbff;
+  background-color: ${(props) => props.bg};
   padding: 40px 0;
-
-  h1 {
-    color: red;
-  }
-
-  &:hover {
-    background-color: black;
-  }
 `;
