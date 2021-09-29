80ef24de05ea18abff3ce0abe3bf7beb86f2a9f3
Troy Dorman <troydorman@gmail.com>
2021-09-29 08:37:32 -0600

    implemented theme provider

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import Header from "./components/Header";
 import { Container } from "./components/styles/Container.styled";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    header: "#ebfbff",
+    body: "#fff",
+    footer: "#003333",
+  },
+};
 
 function App() {
   return (
-    <>
-      <Header />
-      <Container>
-        <h1>hello world</h1>
-      </Container>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Header />
+        <Container>
+          <h1>hello world</h1>
+        </Container>
+      </>
+    </ThemeProvider>
   );
 }
 
